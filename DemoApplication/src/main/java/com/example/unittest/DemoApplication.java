a5939bc5a673eefdd5d181e8f24087fe9345eb84
Bilal Hayat <tpaliwal@xebia.com>
2019-09-07 18:44:20 +0530

    for builder pattern

--- DemoApplication/src/main/java/com/example/unittest/DemoApplication.java
+++ DemoApplication/src/main/java/com/example/unittest/DemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.unittest;
 
+import com.example.unittest.model.EmployeeBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		EmployeeBuilder emp_1 = new EmployeeBuilder.Builder().withEid(1).build();
+
+		System.out.println("=========  emp_1 contains :" + emp_1.toString());
+
+		emp_1 = new EmployeeBuilder.Builder()
+				                   .withEid(1)
+				                   .withFirstname("Builder pattern")
+				                   .build();
+
+		System.out.println("=========  emp_1 contains :" + emp_1.toString());
+
+		emp_1 = new EmployeeBuilder.Builder()
+				.withEid(1)
+				.withFirstname("Builder pattern")
+				.withlastName("Third Object")
+				.build();
+
+		System.out.println("=========  emp_1 contains :" + emp_1.toString());
+
+
+
 	}
+
+
+
 
 }
