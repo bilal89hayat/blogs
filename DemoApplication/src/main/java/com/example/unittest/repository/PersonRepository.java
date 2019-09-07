26c3f166c4ac356917c116343b26513b88e7e467
Bilal Hayat <tpaliwal@xebia.com>
2019-09-07 18:48:45 +0530

    removing unwanted files

--- DemoApplication/src/main/java/com/example/unittest/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.unittest.repository;
-
-import com.example.unittest.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
